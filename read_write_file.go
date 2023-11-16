890056ef2700828f0bc19c71af5981e5cf7e2a90
saltkid <ojisaltkid@gmail.com>
2023-11-16 16:16:56 +0800

    appending to files yoooooo

--- read_write_file.go
+++ read_write_file.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
 )
 
@@ -12,7 +13,7 @@ func write_to_file(filename string) {
 	}
 	defer file.Close()
 
-	_, write_err := file.WriteString("hello file")
+	_, write_err := fmt.Fprintln(file, "hello file")
 	if write_err != nil {
 		panic(write_err)
 	}
@@ -34,11 +35,51 @@ func write_lines_to_file(filename string, data []string) {
 	}
 }
 
-func ReadWriteFile() {
-	data := []string{
-		"hello",
-		"file",
+func append_to_file(filename string, data string) {
+	file, err := os.OpenFile(filename,
+							os.O_APPEND|os.O_WRONLY,
+							fs.ModeAppend)
+	if err != nil {
+		panic(err)
 	}
-	write_to_file("test_write_file.txt")
-	write_lines_to_file("test_write_lines_to_file.txt", data)
+	defer file.Close()
+
+	_, append_error := fmt.Fprintln(file, data)
+	if append_error != nil {
+		panic(append_error)
+	}
+}
+
+func append_lines_to_file(filename string, data []string) {
+	file, err := os.OpenFile(filename,
+							os.O_APPEND|os.O_WRONLY,
+							fs.ModeAppend)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	for _, line := range data {
+		_, append_error := fmt.Fprintln(file, line)
+		if append_error != nil {
+			panic(append_error)
+		}
+	}
+}
+
+func ReadWriteFile() {
+	// data := []string{
+	// 	"hello",
+	// 	"file",
+	// }
+	// write_to_file("test_write_file.txt")
+	// write_lines_to_file("test_write_lines_to_file.txt", data)
+	
+	append_to_file("test_write_file.txt", "i am a new line that was appended")
+
+	append_data := []string {
+		"append",
+		"THIS",
+	}
+	append_lines_to_file("test_write_lines_to_file.txt", append_data)
 }
